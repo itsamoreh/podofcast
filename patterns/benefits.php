1e9f2b046922e2bf611332a5e32d912050019596
Amor Kumar <contact@amorkumar.com>
2023-05-17 21:25:11 -0400

    Add benefits section

---
+++ patterns/benefits.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Title: Benefits
+ * Slug: podofcast/benefits
+ * Description: List of benefits.
+ * Categories: other
+ * Block Types: core/template-part/benefits
+ */
+?>
+<!-- wp:group {"className":"podofcast-pattern-benefits","align":"full","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="podofcast-pattern-benefits wp-block-group alignfull"><!-- wp:heading {"textAlign":"center","fontSize":"h1"} -->
+<h2 class="wp-block-heading has-text-align-center has-h-1-font-size"><strong>Membership benefits</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"davys-grey","fontSize":"h3"} -->
+<p class="has-davys-grey-color has-text-color has-h-3-font-size">Become our sponsor and get all benefits</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"className":"is-style-default","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-group is-style-default"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":135,"width":78,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost:8888/wp-content/themes/podofcast/assets/images/benefits-scribble.png" alt="" class="wp-image-135" width="78"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"h3"} -->
+<h3 class="wp-block-heading has-text-align-center has-h-3-font-size"><strong>Topic by Request</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"Body"} -->
+<p class="has-text-align-center has-body-font-size">Lorem ipsum dolor sit amet consectet pis<br>cing elit, sed do eiusmod tempor.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":136,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8888/wp-content/themes/podofcast/assets/images/benefits-shining-stars.png" alt="" class="wp-image-136"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"h3"} -->
+<h3 class="wp-block-heading has-text-align-center has-h-3-font-size"><strong>Exclusive Content</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"Body"} -->
+<p class="has-text-align-center has-body-font-size">Lorem ipsum dolor sit amet consectet pis<br>cing elit, sed do eiusmod tempor.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":137,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8888/wp-content/themes/podofcast/assets/images/benefits-face-1.png" alt="" class="wp-image-137"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"h3"} -->
+<h3 class="wp-block-heading has-text-align-center has-h-3-font-size"><strong>Join The Community</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"Body"} -->
+<p class="has-text-align-center has-body-font-size">Lorem ipsum dolor sit amet consectet pis<br>cing elit, sed do eiusmod tempor.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":138,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8888/wp-content/themes/podofcast/assets/images/benefits-face-2.png" alt="" class="wp-image-138"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"h3"} -->
+<h3 class="wp-block-heading has-text-align-center has-h-3-font-size"><strong>Livestreaming Access</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"Body"} -->
+<p class="has-text-align-center has-body-font-size">Lorem ipsum dolor sit amet consectet pis<br>cing elit, sed do eiusmod tempor.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":139,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8888/wp-content/themes/podofcast/assets/images/benefits-fire.png" alt="" class="wp-image-139"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"h3"} -->
+<h3 class="wp-block-heading has-text-align-center has-h-3-font-size"><strong>Exclusive Episodes &amp; Merch</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"Body"} -->
+<p class="has-text-align-center has-body-font-size">Lorem ipsum dolor sit amet consectet pis<br>cing elit, sed do eiusmod tempor.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":140,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8888/wp-content/themes/podofcast/assets/images/benefits-star.png" alt="" class="wp-image-140"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"h3"} -->
+<h3 class="wp-block-heading has-text-align-center has-h-3-font-size"><strong>And much more!</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"Body"} -->
+<p class="has-text-align-center has-body-font-size">Lorem ipsum dolor sit amet consectet pis<br>cing elit, sed do eiusmod tempor.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"left":"var:preset|spacing|x-large","right":"var:preset|spacing|x-large","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--x-large)">See Pricing</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
